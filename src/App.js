c58f9d40ba8abc9023176ac5c66f9ca54dd5e984
Leonidas Yopan <leonidasyopan@gmail.com>
2020-04-08 12:21:16 -0300

    Properly commented the steps

--- src/App.js
+++ src/App.js
@@ -5,42 +5,54 @@ import "./styles.css";
 
 function App() {
 
+  // This is a variable that stores all the projects.
+  // It's only used this way because we haven's created a database for it yet.
   const [repositories, setRepositories] = useState([]);  
   
+  // useEffect is responsible for accessing the API (backend), bringing the data available 
+  // and storing this data into our immutable variable - it uses the setRepositories function
+  // in order to be able to 're-create' the variable, since it's immutable.
   useEffect(() => {
     api.get('/repositories').then(response => {
       setRepositories(response.data);
     });
   }, []);
 
+  // async Function responsible for adding a new repository into the list.
+  // At this moment it's static information (not dynamic).
   async function handleAddRepository() {
+    // this first variable access the POST root for adding a new repository
     const response =  await api.post('repositories', {
       title: "Desafio React.js 04",
       url: "http://github.com/test04",
       techs: ["React", "Node.js"]
     })
 
+    // this variable grabs the data create above and stored in the variable above
     const repository = response.data;
 
+    // then the useState() function is run to re-create our list with the new item
     setRepositories([...repositories, repository]);
   }
 
+  // Very simple async funtion responsible for deleting a specific item from the list.
+  // It receives as a parameter the parameter of the chosen repository.
   async function handleRemoveRepository(id) {
-    const response = await api.delete(`repositories/${id}`)
-    console.log(repositories);
+    // Access the DELETE route from the API passing the ID of the repository as parameter.
+    await api.delete(`repositories/${id}`)
 
+    // Uses the setRepository function - which is a parameter of the useState() to re-create
+    // our list of repositories. In this case we want to remove the repository with the provided
+    // ID, that's why we use filter excluding any repository that happens to hold that it.
     setRepositories(repositories.filter(repository => repository.id !== id));
-/*
-    api.get('/repositories').then(response => {
-      setRepositories(response.data);
-    });
-*/
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        
+
+          { /* This map() method is very interesting. In this case it's allowing us to iterate through each item
+          of our list of repositories. For each item it's creating a List Item (<li>) inside the <ul> */ }
           {repositories.map(repository => <li key={repository.id}>{repository.title}
           
             <button onClick={() => handleRemoveRepository(repository.id)}>
